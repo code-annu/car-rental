c79e917b80acdf5fdd043ceb81faf03999d45986
Anurag Rajput <code.annu@gmail.com>
2025-08-05 10:28:32 +0530

    Add tailwind and config tailwind

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import path from "path";
 
 const app: Express = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to typescript project!");
+  res.render("pages/home");
 });
 
 app.listen(PORT, () => {
